76a0ed2e7f8df8319a035ac81ac380aff8dab475
dxwebster <adrianalimafm@gmail.com>
2020-08-27 22:12:57 -0300

    Implementação de Filtro

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Make Cards
-const sectionCards = document.querySelector("section.cards");
+const sectionCards = document.querySelector("div.cards");
 
 const card = document.querySelector("div.card");
 
@@ -8,44 +8,64 @@ const videos = [
     title: "Formulário Animado com JS puro e CSS Animation | Mayk Brito",
     duration: "57 min",
     thumb: "https://i.ytimg.com/vi/GykTLqODQuU/hqdefault.jpg",
-    video_id: "GykTLqODQuU"
+    video_id: "GykTLqODQuU",
+    video_class: "card mb"
   },
   {
     title: "Desvendando o CSS Grid na prática | Mayk Brito",
     duration: "36 min",
     thumb: "https://i.ytimg.com/vi/HN1UjzRSdBk/hqdefault.jpg",
-    video_id: "HN1UjzRSdBk"
+    video_id: "HN1UjzRSdBk",
+    video_class: "card vga"
   },
   {
     title: "Array: Higher Order Functions | Mayk Brito",
     duration: "54 min",
     thumb: "https://i.ytimg.com/vi/rAzHvYnQ8DY/hqdefault.jpg",
-    video_id: "rAzHvYnQ8DY"
+    video_id: "rAzHvYnQ8DY",
+    video_class: "card lcd"
   },
   {
     title: "O que é API? REST e RESTful? | Mayk Brito",
     duration: "33 min",
     thumb: "https://i.ytimg.com/vi/ghTrp1x_1As/hqdefault.jpg",
-    video_id: "ghTrp1x_1As"
+    video_id: "ghTrp1x_1As",
+    video_class: "card mb wir"
   },
   {
     title: "Desvendando a variável this no Javascript | Mayk Brito",
     duration: "48 min",
     thumb: "https://i.ytimg.com/vi/GSqR2i-Pq6o/hqdefault.jpg",
-    video_id: "GSqR2i-Pq6o"
+    video_id: "GSqR2i-Pq6o",
+    video_class: "card mb"
   },
   {
-    title:
-      "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
+    title: "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
     duration: "33 min",
     thumb: "https://i.ytimg.com/vi/2alg7MQ6_sI/hqdefault.jpg",
-    video_id: "2alg7MQ6_sI"
+    video_id: "2alg7MQ6_sI",
+    video_class: "card mb"
+  },
+  {
+    title: "Desvendando a variável this no Javascript | Mayk Brito",
+    duration: "48 min",
+    thumb: "https://i.ytimg.com/vi/GSqR2i-Pq6o/hqdefault.jpg",
+    video_id: "GSqR2i-Pq6o",
+    video_class: "card mb"
+  },
+  {
+    title: "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
+    duration: "33 min",
+    thumb: "https://i.ytimg.com/vi/2alg7MQ6_sI/hqdefault.jpg",
+    video_id: "2alg7MQ6_sI",
+    video_class: "card mb"
   }
 ];
 
 videos.map(video => {
   const cardClone = card.cloneNode(true);
   cardClone.setAttribute("id", video.video_id);
+  cardClone.setAttribute("class", video.video_class);
   cardClone.querySelector("img").src = video.thumb;
   cardClone.querySelector(".title").innerHTML = video.title;
   cardClone.querySelector(".info > p.text--medium").innerHTML =
@@ -54,6 +74,8 @@ videos.map(video => {
 });
 
 card.remove();
+
+
 
 // Modal actions
 const modalOverlay = document.querySelector(".modal-overlay");
@@ -80,5 +102,3 @@ document.querySelector(".close-modal").addEventListener("click", () => {
 
 
 
-
-// MENU RESPONSIVO
